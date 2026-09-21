f8b48eac991d22b4ed7280e560e7568da9534479
Miles Angela <milespinatil17@gmail.com>
2026-09-21 18:18:51 +0800

    Round shift pay calculation - pinatil.angela

--- shifts.js
+++ shifts.js
@@ -4,10 +4,10 @@ function isValidShift(hours) {
 
 function calculatePay(hours, rate) {
   if (hours > 8) {
-    return Math.floor((8 * rate) + ((hours - 8) * rate * 1.5));
+    return Math.round((8 * rate) + ((hours - 8) * rate * 1.5));
   }
 
-  return Math.floor(hours * rate);
+  return Math.round(hours * rate);
 }
 
 module.exports = { isValidShift, calculatePay };
