4f2813d1172731b0f6ee4fb86652cb9e377fb8db
Miles Angela <milespinatil17@gmail.com>
2026-09-21 18:12:23 +0800

    Add overtime pay calculation - pinatil.angela

--- shifts.js
+++ shifts.js
@@ -3,6 +3,10 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
+  if (hours > 8) {
+    return Math.floor((8 * rate) + ((hours - 8) * rate * 1.5));
+  }
+
   return Math.floor(hours * rate);
 }
 
