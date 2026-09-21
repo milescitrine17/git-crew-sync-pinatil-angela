6a651ab823f8d42cea9e9e4d9811c66397791628
Miles Angela <milespinatil17@gmail.com>
2026-09-21 19:27:02 +0800

    Update shift rounding behavior - pinatil.angela

--- shifts.js
+++ shifts.js
@@ -7,7 +7,7 @@ function calculatePay(hours, rate) {
     return Math.round((8 * rate) + ((hours - 8) * rate * 1.5));
   }
 
-  return Math.floor(hours * rate);
+  return Math.ceil(hours * rate);
 }
 
 module.exports = { isValidShift, calculatePay };
