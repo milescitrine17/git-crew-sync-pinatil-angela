5e4f0b9b2ab5b1668f1c746c58aaf63f31e51c13
Miles Angela <milespinatil17@gmail.com>
2026-09-21 18:25:05 +0800

    Round overtime pay calculation - pinatil.angela

--- shifts.js
+++ shifts.js
@@ -4,7 +4,7 @@ function isValidShift(hours) {
 
 function calculatePay(hours, rate) {
   if (hours > 8) {
-    return Math.floor((8 * rate) + ((hours - 8) * rate * 1.5));
+    return Math.round((8 * rate) + ((hours - 8) * rate * 1.5));
   }
 
   return Math.floor(hours * rate);
